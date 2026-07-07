ca7c876946f5028c686ce1fdb6ec8c39fa37c175
AldienHW <aldienhaidarwahyono@gmail.com>
2026-07-07 21:03:25 +0700

    Adding login page

--- resources/js/Components/User/Navbar.tsx
+++ resources/js/Components/User/Navbar.tsx
@@ -4,7 +4,7 @@ import ProfileIcon from "../../../../assets/download-removebg-preview.png"
 export default function Navbar({ children }: PropsWithChildren) {
     return(
     <>
-        <nav className="bg-[#FF5454] fixed w-full m-0 p-3 px-6 flex justify-between items-center">
+        <nav className="bg-[#FF5454] shadow-[0px_0px_10px_black] fixed w-full m-0 p-3 px-6 flex justify-between items-center">
             <h1 className="font-freckle text-3xl text-white">Logbook</h1>
             <div className='flex items-center w-200 justify-between ml-auto'>{children}</div>
             <img src={ProfileIcon} alt="UserIcon" width={"70rem"} />
