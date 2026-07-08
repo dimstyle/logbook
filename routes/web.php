a9b5d33259b3403b100237a2ac0d668db3725b2c
Mwantap <raffa3agas.1212@gmail.com>
2026-07-08 15:30:32 +0700

    add: Report, EditReport, Clock-Out

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,4 @@ Route::get('/login', fn() => Inertia::render('Login'));
 Route::get('/editreport', fn() => Inertia::render('EditReport'));
 Route::get('/clock-in', fn() => Inertia::render('Attendance_Clock-In'));
 Route::get('/report', fn() => Inertia::render('Attendance_ActivityReport'));
-
+Route::get('/clock-out', fn() => Inertia::render('Attendance_Clock-Out'));
