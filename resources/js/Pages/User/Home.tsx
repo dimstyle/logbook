bdfde927e2ef4200dc3c107465982ba9d93231de
003-aldienhw <aldienhaidarwahyono@gmail.com>
2026-07-08 10:15:29 +0700

    Adding clock-in page

--- resources/js/Pages/User/Home.tsx
+++ resources/js/Pages/User/Home.tsx
@@ -5,10 +5,10 @@ export default function Home(){
         <>
             <Navbar>
                 <input className="w-70 p-1.5 bg-white rounded-lg" type="text" placeholder="Search Reports" />
-                <div className="flex justify-between w-60 mr-2 text-white">
-                    <a href="">History</a>
-                    <a href="">Attendance</a>
-                    <a href="">Logout</a>
+                <div className="flex justify-between items-center w-60 mr-2 text-white">
+                    <a href="" className="bg-white text-black p-1 rounded-lg">History</a>
+                    <a href="/clock-in" className="p-1">Attendance</a>
+                    <a href="/login" className="p-1">Logout</a>
                 </div>
             </Navbar>
             <div className="p-4 pt-25">
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,4 @@ use Inertia\Inertia;
 
 Route::get('/', fn() => Inertia::render('Home'));
 Route::get('/login', fn() => Inertia::render('Login'));
+Route::get('/clock-in', fn() => Inertia::render('Attendance_Clock-In'));
