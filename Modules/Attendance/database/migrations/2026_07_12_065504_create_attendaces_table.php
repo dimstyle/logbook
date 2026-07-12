d15b51254f8c29d8708394429e2b36d685737e1b
dimstyle <smilehacking27@gmail.com>
2026-07-12 14:11:36 +0700

    add: attendande module and table

---
+++ Modules/Attendance/database/migrations/2026_07_12_065504_create_attendaces_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('attendaces', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('account_id')->constrained();
+
+            $table->boolean('sudah_hadir');
+            $table->time('jam_hadir');
+            
+            $table->boolean('sudah_pulang');
+            $table->time('jam_pulang');
+            
+            $table->boolean('wfh');
+            
+            $table->boolean('sudah_laporan');
+            $table->string('laporan');
+            $table->json('images_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendaces');
+    }
+};
