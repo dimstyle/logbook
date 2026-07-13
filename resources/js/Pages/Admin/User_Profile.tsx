be8ebac7cebe660f12783561d8feb29389ca1ef4
003-aldienhw <aldienhaidarwahyono@gmail.com>
2026-07-13 16:00:19 +0700

    Adding user profile on admin page

---
+++ resources/js/Pages/Admin/User_Profile.tsx
@@ -0,0 +1,78 @@
+import AdminNavbar from "../../Components/Admin/Navbar.js";
+import ProfileIcon from "../../../../assets/download-removebg-preview.png";
+
+export default function UserProfileOnAdmin() {
+    return (
+        <>
+            <AdminNavbar>
+                <div className="w-full justify-start" />
+                <div className="flex gap-2 items-center w-60 mr-7 text-white">
+                    <a href="/admin/user-list" className="p-1">Users</a>
+                    <a href="/admin/user-registration" className="p-1">Registration</a>
+                    <a href="/admin/daily-attendance" className="p-1">Attendance</a>
+                </div>
+            </AdminNavbar>
+            <div className="p-4 pl-40 pr-40 pt-30">
+                <div className="bg-[#F4F4F4] w-full p-10 rounded-xl">
+                    <div className="flex items-center">
+                        <img src={ProfileIcon} alt="UserIcon" />
+                        <div className="flex flex-col w-full gap-8 ml-5">
+                            <h1 className="text-3xl">Dimas sabunan</h1>
+                            <h2 className="text-[#1D4ED8] text-xl">Siswa SMK</h2>
+                        </div>
+                        <div className="flex w-full justify-end mr-10">
+                            <a href="" className="flex items-center gap-2 bg-[#FFC7C7] p-2 rounded-xl text-[#FF5454]">Delete Account</a>
+                        </div>
+                    </div>
+                    <div className="flex flex-col mx-5 mt-20">
+                        <h1 className="text-xl">Informasi siswa</h1>
+                        <div className="flex gap-20 mt-10">
+                            <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
+                                <h1 className="text-xl text-[#666]">Sekolah</h1>
+                                <h1>Di sono</h1>
+                            </div>
+                            <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
+                                <h1 className="text-xl text-[#666]">Jurusan</h1>
+                                <h1>Akadub</h1>
+                            </div>
+                        </div>
+                        <div className="flex gap-20 mt-10">
+                            <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
+                                <h1 className="text-xl text-[#666]">Email</h1>
+                                <h1>Dimassabunan@gmail.com</h1>
+                            </div>
+                            <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
+                                <h1 className="text-xl text-[#666]">Nomor HP</h1>
+                                <h1>911</h1>
+                            </div>
+                        </div>
+                        <div className="flex gap-20 mt-10">
+                            <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
+                                <h1 className="text-xl text-[#666]">Username</h1>
+                                <h1>dimss1945</h1>
+                            </div>
+                            <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
+                                <h1 className="text-xl text-[#666]">Password</h1>
+                                <h1>Gak tau</h1>
+                            </div>
+                        </div>
+                        <div className="flex gap-30 mt-20 mb-10">
+                            <div className="flex flex-col items-center bg-[#FFC7C7] w-full rounded-lg p-4 py-10">
+                                <h1 className="text-xl text-[#FF5454]">0</h1>
+                                <h1>Hadir</h1>
+                            </div>
+                            <div className="flex flex-col items-center bg-[#FFC7C7] w-full rounded-lg p-4 py-10">
+                                <h1 className="text-xl text-[#FF5454]">100</h1>
+                                <h1>Tidak Masuk</h1>
+                            </div>
+                            <div className="flex flex-col items-center bg-[#FFC7C7] w-full rounded-lg p-4 py-10">
+                                <h1 className="text-xl text-[#FF5454]">0</h1>
+                                <h1>Laporan</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
