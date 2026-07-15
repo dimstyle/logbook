8e16c4753654a3335aca8b18658a70b2615017e8
003-aldienhw <aldienhaidarwahyono@gmail.com>
2026-07-15 11:50:26 +0700

    Finishing edit profile page

--- resources/js/Components/Admin/Navbar.tsx
+++ resources/js/Components/Admin/Navbar.tsx
@@ -4,7 +4,7 @@ import ProfileIcon from "../../../../assets/download-removebg-preview.png"
 export default function AdminNavbar({ children }: PropsWithChildren) {
     return(
     <>
-        <nav className="bg-[#FF5454] shadow-[0px_0px_10px_black] fixed w-full m-0 p-3 px-6 flex justify-between items-center">
+        <nav className="bg-[#FF5454] shadow-[0px_0px_10px_black] z-1000 fixed w-full m-0 p-3 px-6 flex justify-between items-center">
             <div className='flex items-end'>
                 <h1 className="font-freckle text-3xl text-white">Logbook</h1>
                 <h1 className="font-freckle text-xl text-[#560000]">Admin</h1>
--- resources/js/Pages/User/User_Profile_Edit.tsx
+++ resources/js/Pages/User/User_Profile_Edit.tsx
@@ -7,18 +7,40 @@ export default function UserProfileEdit() {
             <Navbar>
                 <div className="w-full justify-start" />
                 <div className="flex gap-2 items-center w-60 mr-2 text-white">
-                    <a href="" className="p-1">History</a>
+                    <a href="/" className="p-1">History</a>
                     <a href="/clock-in" className="p-1">Attendance</a>
                     <a href="/login" className="p-1">Logout</a>
                 </div>
             </Navbar>
             <div className="p-4 pl-40 pr-40 pt-30">
                 <div className="bg-[#F4F4F4] w-full p-10 rounded-xl">
-                    <div className="flex items-center">
-                        <img src={ProfileIcon} alt="UserIcon" />
-                        <div className="flex flex-col w-full gap-8 ml-5">
-                            <h1 className="text-3xl"></h1>
-                            <h2 className="text-[#1D4ED8] text-xl"></h2>
+                    <div className="flex flex-col w-full items-center">
+                        <label className="group relative w-60 rounded-full overflow-hidden cursor-pointer">
+                            <input 
+                                type="file" 
+                                accept="image/*" 
+                                className="hidden" 
+                            />
+                            <img 
+                                className="w-full object-cover aspect-square transition-all duration-300 group-hover:scale-105" 
+                                src={ProfileIcon} 
+                                alt="UserIcon"
+                            />
+                            <div className="absolute inset-0 bg-black/50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                                <svg 
+                                    className="h-8 w-8 text-white" 
+                                    fill="none" 
+                                    viewBox="0 0 24 24" 
+                                    stroke="currentColor" 
+                                    stroke-width="2"
+                                >
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                                </svg>
+                            </div>
+                        </label>
+                        <div className="flex flex-col w-full gap-4 ml-5 mt-10">
+                            <h1 className="text-2xl">Nama</h1>
+                            <input type="text" className="w-150 p-1.5 bg-[#666] rounded-lg text-white" />
                         </div>
                     </div>
                     <div className="flex flex-col mx-5 mt-20">
@@ -26,45 +48,31 @@ export default function UserProfileEdit() {
                         <div className="flex gap-20 mt-10">
                             <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
                                 <h1 className="text-xl text-[#666]">Sekolah</h1>
-                                <h1></h1>
+                                <input type="text" className="w-full p-1.5 bg-[#666] rounded-lg text-white" />
                             </div>
                             <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
                                 <h1 className="text-xl text-[#666]">Jurusan</h1>
-                                <h1></h1>
+                                <input type="text" className="w-full p-1.5 bg-[#666] rounded-lg text-white" />
                             </div>
                         </div>
                         <div className="flex gap-20 mt-10">
                             <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
                                 <h1 className="text-xl text-[#666]">Email</h1>
-                                <h1></h1>
+                                <input type="text" className="w-full p-1.5 bg-[#666] rounded-lg text-white" />
                             </div>
                             <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
                                 <h1 className="text-xl text-[#666]">Nomor HP</h1>
-                                <h1></h1>
+                                <input type="text" className="w-full p-1.5 bg-[#666] rounded-lg text-white" />
                             </div>
                         </div>
                         <div className="flex gap-20 mt-10">
                             <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
                                 <h1 className="text-xl text-[#666]">Username</h1>
-                                <h1></h1>
+                                <input type="text" className="w-full p-1.5 bg-[#666] rounded-lg text-white" />
                             </div>
                             <div className="bg-gray-200 w-full border-2 border-[#999] rounded-lg p-4">
                                 <h1 className="text-xl text-[#666]">Password</h1>
-                                <h1></h1>
-                            </div>
-                        </div>
-                        <div className="flex gap-30 mt-20 mb-10">
-                            <div className="flex flex-col items-center bg-[#FFC7C7] w-full rounded-lg p-4 py-10">
-                                <h1 className="text-xl text-[#FF5454]"></h1>
-                                <h1>Hadir</h1>
-                            </div>
-                            <div className="flex flex-col items-center bg-[#FFC7C7] w-full rounded-lg p-4 py-10">
-                                <h1 className="text-xl text-[#FF5454]"></h1>
-                                <h1>Tidak Masuk</h1>
-                            </div>
-                            <div className="flex flex-col items-center bg-[#FFC7C7] w-full rounded-lg p-4 py-10">
-                                <h1 className="text-xl text-[#FF5454]"></h1>
-                                <h1>Laporan</h1>
+                                <input type="text" className="w-full p-1.5 bg-[#666] rounded-lg text-white" />
                             </div>
                         </div>
                     </div>
