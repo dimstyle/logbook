1f5b3c0a2df09c3f66117b216696aa781a708258
dimstyle <smilehacking27@gmail.com>
2026-07-15 14:08:58 +0700

    refactor: Browser Route

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,44 @@ $mockuser = [
 
 // User
 
-Route::get('/', fn() => Inertia::render('User/Home'));
 Route::get('/login', fn() => Inertia::render('User/Login'));
-Route::get('/clock-in', fn() => Inertia::render('User/Attendance_Clock-In'));
-Route::get('/user_profile', fn() => Inertia::render('User/User_Profile'));
-Route::get('/report', fn() => Inertia::render('User/Attendance_ActivityReport'));
-Route::get('/clock-out', fn() => Inertia::render('User/Attendance_Clock-Out'));
-Route::get('/edit_report', fn() => Inertia::render('User/EditReport'));
-Route::get('/view_report', fn() => Inertia::render('User/ViewReport'));
+
+Route::prefix('/')
+->middleware('jwt.page.validation') 
+->group(function (){
+    Route::get('/', fn() => Inertia::render('User/Home'));
+    Route::get('/clock-in', fn() => Inertia::render('User/Attendance_Clock-In'));
+    Route::get('/user_profile', fn() => Inertia::render('User/User_Profile'));
+    Route::get('/report', fn() => Inertia::render('User/Attendance_ActivityReport'));
+    Route::get('/clock-out', fn() => Inertia::render('User/Attendance_Clock-Out'));
+    Route::get('/edit_report', fn() => Inertia::render('User/EditReport'));
+    Route::get('/view_report', fn() => Inertia::render('User/ViewReport'));
+});
+
 
 // Admin
+Route::prefix('admin')
+->group(function (){
+    Route::get('/login', fn() => Inertia::render('Admin/Login'));
+});
 
-Route::get('/admin/login', fn() => Inertia::render('Admin/Login'));
-Route::get('/admin/daily_attendance', fn() => Inertia::render('Admin/Daily_Attendance'));
-Route::get('/admin/profile', fn() => Inertia::render('Admin/Admin_Profile'));
-Route::get('/admin/user_registration', fn() => Inertia::render('Admin/User_Registration'));
-Route::get('/admin/user_list', fn() => Inertia::render('Admin/User_List'));
-Route::get('/admin/user_profile/{id}', function ($id) use ($mockuser) {
-    $selectedUser = collect($mockuser)->firstWhere('id', (int)$id);
-
-    if (!$selectedUser) {
-        abort(404, 'User Not Found.');
-    }
-
-    return Inertia::render('Admin/User_Profile', [
-        'user' => $selectedUser
-    ]);
+Route::prefix('admin')
+->middleware('jwt.page.validation')
+->group(function() {
+    global $mockuser;
+    Route::get('/daily_attendance', fn() => Inertia::render('Admin/Daily_Attendance'));
+    Route::get('/profile', fn() => Inertia::render('Admin/Admin_Profile'));
+    Route::get('/user_registration', fn() => Inertia::render('Admin/User_Registration'));
+    Route::get('/user_list', fn() => Inertia::render('Admin/User_List'));
+    Route::get('/user_profile/{id}', function ($id) use ($mockuser) {
+        $selectedUser = collect($mockuser)->firstWhere('id', (int)$id);
+    
+        if (!$selectedUser) {
+            abort(404, 'User Not Found.');
+        }
+    
+        return Inertia::render('Admin/User_Profile', [
+            'user' => $selectedUser
+        ]);
+    });
 });
