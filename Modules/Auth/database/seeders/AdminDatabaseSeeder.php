7ee7ae25608ea02a73633613668a059cdac7d596
Mwantap <raffa3agas.1212@gmail.com>
2026-07-22 09:51:32 +0700

    p

--- Modules/Auth/database/seeders/AdminDatabaseSeeder.php
+++ Modules/Auth/database/seeders/AdminDatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Modules\Auth\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Modules\Auth\Models\Account;
 use Modules\User\Models\Admin;
+use Illuminate\Support\Facades\Hash;
 
 class AdminDatabaseSeeder extends Seeder
 {
@@ -45,7 +46,7 @@ class AdminDatabaseSeeder extends Seeder
                 ['email' => $data['email']],
                 Account::factory()->raw([
                     'username' => $data['username'],
-                    'email' => $data['email'],
+                    'password' => Hash::make('password'),
                     'role' => 'admin',
                 ])
             );
