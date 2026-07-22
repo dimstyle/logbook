b20fddde79a092609d4b89993fb7f7e3c5e21dda
dimstyle <smilehacking27@gmail.com>
2026-07-22 13:20:08 +0700

    add: delete feature

--- Modules/Auth/app/Http/Controllers/DeleteAccountController.php
+++ Modules/Auth/app/Http/Controllers/DeleteAccountController.php
@@ -5,8 +5,10 @@ namespace Modules\Auth\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Modules\Auth\Services\DeleteAccountService;
+use PHPUnit\TextUI\Output\SummaryPrinter;
 use Symfony\Component\HttpFoundation\Response;
 use Throwable;
+use OpenApi\Attributes as OA;
 
 class DeleteAccountController extends Controller
 {
@@ -14,6 +16,33 @@ class DeleteAccountController extends Controller
         private DeleteAccountService $deleteAccountService
     ){}
 
+
+    #[OA\Post(
+        path: "/api/auth/deleteaccount",
+        summary: "Delete user account",
+        tags: ["Auth"],
+    )]
+    #[OA\Response(
+        response: 204,
+        description: "Success to delete account",
+        content: new OA\JsonContent(
+            ref: "#/components/schemas/RefreshTokenResponse",
+        )
+    )]
+    #[OA\Response(
+        response: 401,
+        description: "Unauthorized",
+        content: new OA\JsonContent(
+            ref: "#/components/schemas/DefaultResponse"
+        )
+    )]
+    #[OA\Response(
+        response: 500,
+        description: "Internal server error",
+        content: new OA\JsonContent(
+            ref: "#/components/schemas/DefaultResponse"
+        )
+    )]
     public function handle($id){   
         try{    
             $this->deleteAccountService->handle($id);
--- Modules/Auth/app/Http/Controllers/LogoutController.php
+++ Modules/Auth/app/Http/Controllers/LogoutController.php
@@ -6,8 +6,21 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cookie;
 use Symfony\Component\HttpFoundation\Response;
 
+use OpenApi\Attributes as OA;
 class LogoutController extends Controller
 {
+    #[OA\Post(
+        path: "/api/auth/logout",
+        summary: "Logout from account",
+        tags: ["Auth"]  
+    )]
+    #[OA\Response(
+        response: 200,
+        description: "Success logout",
+        content: new OA\JsonContent(
+            ref: "#/components/schemas/DefaultResponse",
+        )
+    )]
     public function handle(){
         return response()->json([
             'message' => 'Success to logout'
